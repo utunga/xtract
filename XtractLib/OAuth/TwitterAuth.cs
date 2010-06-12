1809136933433d195c68331f7dc12c81a6ddc1a6
Miles Thompson <utunga@gmail.com>
2010-06-12 15:15:58 +1200

    added OAuth classes

---
+++ XtractLib/OAuth/TwitterAuth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace XtractLib.OAuth
+{
+    public static class TwitterAuth
+    {
+        const string AUTH_KEY = "oauth";
+        const string USER_KEY = "oauth_user";
+
+        public static bool HasValidSession()
+        {
+            return ((HttpContext.Current != null) &&
+                    (HttpContext.Current.Session[AUTH_KEY] != null) &&
+                    (HttpContext.Current.Session[USER_KEY] != null));
+        }
+
+        public static OAuthTwitter CurrentSession
+        {
+            get 
+            {
+                if ((HttpContext.Current != null) &&
+                    (HttpContext.Current.Session[AUTH_KEY] != null))
+                {
+                    return (OAuthTwitter)HttpContext.Current.Session[AUTH_KEY];
+                }
+                return null;
+            }
+        }
+
+
+    }
+}
